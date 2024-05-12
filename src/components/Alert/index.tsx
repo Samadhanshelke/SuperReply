5564ae9ed981f7fdc8cb7aef22c30a8bbf3fdb5d
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2024-05-12 18:27:13 +0530

    first commit

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,28 +1,29 @@
 // GlobalAlert.js
 import React from 'react';
-import { useAlert } from '@/contexts/AlertContext'; // Adjust the path as per your project structure
+// import { useAlert } from '@/contexts/AlertContext'; // Adjust the path as per your project structure
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 
 const Alert = () => {
-  const { globalAlert, hideAlert } = useAlert();
+  // const { globalAlert, hideAlert } = useAlert();
 
   return (
-    <Snackbar
-      open={globalAlert.open}
-      autoHideDuration={3000}
-      onClose={hideAlert}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
-      <MuiAlert
-        onClose={hideAlert}
-        severity={globalAlert.severity}
-        sx={{ width: '100%' }}
-      >
-        <AlertTitle>{globalAlert.title}</AlertTitle>
-        {globalAlert.message}
-      </MuiAlert>
-    </Snackbar>
+    // <Snackbar
+    //   open={globalAlert.open}
+    //   autoHideDuration={3000}
+    //   onClose={hideAlert}
+    //   anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+    //   <MuiAlert
+    //     onClose={hideAlert}
+    //     severity={globalAlert.severity}
+    //     sx={{ width: '100%' }}
+    //   >
+    //     <AlertTitle>{globalAlert.title}</AlertTitle>
+    //     {globalAlert.message}
+    //   </MuiAlert>
+    // </Snackbar>
+    <div>hello</div>
   );
 };
 
