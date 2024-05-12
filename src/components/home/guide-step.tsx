076ceec926b7b0d82c9892bacf18d51cfebcaf75
Samadhanshelke <96407937+Samadhanshelke@users.noreply.github.com>
2024-05-12 22:07:34 +0530

    ui change

--- src/components/home/guide-step.tsx
+++ src/components/home/guide-step.tsx
@@ -24,7 +24,7 @@ export const GuideStep: React.FC<StepsSectionProps> = ({stepNum, stepName, descr
                     </div>
                 <div className="flex items-center justify-center">
                     <div className="flex max-w-[600px] flex-col gap-4 md:p-[50px] lg:p-[64px] rounded-[40px] bg-white shadow-[0px_11px_30px_0px_rgba(0,0,0,0.10)]">
-                        <div className="font-sharp-grotesk font-semi-bold gap-2 text-3xl lg:text-[40px]">
+                        <div className="font-sharp-grotesk font-semibold  gap-2 text-3xl lg:text-[40px]">
                             <span>Step </span> <GradientText text={stepNum} />
                             <span> {stepName}</span>
                         </div>
@@ -37,17 +37,18 @@ export const GuideStep: React.FC<StepsSectionProps> = ({stepNum, stepName, descr
             <div className="md:hidden flex w-[100%] flex-col gap-24 pb-10 mx-5 items-center justify-around">
                     <div className="h-[300px] w-[300px] flex items-center justify-center rounded-full gradient-primary-conic">
                         <div className={` ${imageOffsetMobile} translate-y-[50px] `}>
-                            <Image src={image} alt="" className={`rounded-[1.4rem] w-[${width}px] h-[${height}px]`}></Image>
+                            <Image src={image} alt="" className={`rounded-[1.4rem] w-[${width}px] bg-cover h-[${height}px]`}></Image>
                         </div>
                     </div>
                 <div className="flex items-center h-[200px] justify-center">
                     <div className="flex flex-col gap-4 rounded-3xl p-4 shadow-xl">
-                        <div className="font-semi-bold gap-2 font-sharp-grotesk text-xl">
-                            <GradientText text={"Step "} />
+                        <div className="font-semi-bold gap-2 font-sharp-grotesk text-2xl">
+                            {/* <GradientText text={"Step "} /> */}
+                            <span>Step </span>
                             <GradientText text={stepNum} />
                             <span> {stepName}</span>
                         </div>
-                        <p className="text-sm font-sf-pro-display font-medium">
+                        <p className="text-sm font-sf-pro-display font-normal">
                             Encourage user comments on your posts &
                             significantly increase your content&apos;s reach. More
                             comments - More reach - More automated
